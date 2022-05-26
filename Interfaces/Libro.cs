5ce30c561845de745649407d39e93c97eca2982c
MartinezFabian <FabianMartinezDev@gmail.com>
2022-05-26 16:58:37 -0300

    Se agrega el proyecto

---
+++ Interfaces/Libro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Libro : IColeccionable, IComparable<Libro>
+    {
+        private string autor;
+        private string titulo;
+
+        public string Autor { get => autor; set => autor = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+
+        public Libro(string autor, string titulo)
+        {
+            this.autor = autor;
+            this.titulo = titulo;
+        }
+
+        public int CompareTo(Libro otro)
+        {
+            return String.Compare(this.titulo, otro.titulo);
+        }
+
+        public String Describir()
+        {
+            return "Titulo: " + titulo + ", del autor " + autor;
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Libro[] biblioteca = new Libro[3];
+            biblioteca[0] = new Libro("Robert C. Martin", "Clean Code");
+            biblioteca[1] = new Libro("William Hohl", "ARM Assembly Language");
+            biblioteca[2] = new Libro("Marijn Haverbeke", "Eloquent JavaScript");
+
+            Array.Sort(biblioteca);
+
+            foreach (Libro libro in biblioteca)
+            {
+                Console.WriteLine(libro.Describir());
+            }
+        }
+    }
+}
